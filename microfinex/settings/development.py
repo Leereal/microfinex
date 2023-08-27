dcc7541be7d5472de14fa04d3e685bb8d2ef6e01
Liberty34 <liberty.m@fbx-group.com>
2023-08-27 15:29:12 +0200

    feature: add custom user model

--- microfinex/settings/development.py
+++ microfinex/settings/development.py
@@ -7,28 +7,28 @@ DATABASES = {
     }
 }
 
-DB_USERNAME = config("POSTGRES_USER")
-DB_PASSWORD = config("POSTGRES_PASSWORD")
-DB_DATABASE = config("POSTGRES_DB")
-DB_HOST = config("POSTGRES_HOST")
-DB_PORT = config("POSTGRES_PORT")
+# DB_USERNAME = config("POSTGRES_USER")
+# DB_PASSWORD = config("POSTGRES_PASSWORD")
+# DB_DATABASE = config("POSTGRES_DB")
+# DB_HOST = config("POSTGRES_HOST")
+# DB_PORT = config("POSTGRES_PORT")
 
-DB_IS_AVAIL = all([
-    DB_USERNAME,
-    DB_PASSWORD,
-    DB_DATABASE,
-    DB_HOST,
-    DB_PORT
-])
+# DB_IS_AVAIL = all([
+#     DB_USERNAME,
+#     DB_PASSWORD,
+#     DB_DATABASE,
+#     DB_HOST,
+#     DB_PORT
+# ])
 
-if DB_IS_AVAIL:
-    DATABASES = {
-        'default': {
-            "ENGINE": 'django.db.backends.postgresql',
-            "NAME": DB_DATABASE,
-            "USER": DB_USERNAME,
-            "PASSWORD": DB_PASSWORD,
-            "HOST": DB_HOST,
-            "PORT": DB_PORT
-        }
-    }
+# if DB_IS_AVAIL:
+#     DATABASES = {
+#         'default': {
+#             "ENGINE": 'django.db.backends.postgresql',
+#             "NAME": DB_DATABASE,
+#             "USER": DB_USERNAME,
+#             "PASSWORD": DB_PASSWORD,
+#             "HOST": DB_HOST,
+#             "PORT": DB_PORT
+#         }
+#     }
--- microfinex/urls.py
+++ microfinex/urls.py
@@ -4,5 +4,25 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("supersecret/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.contrib import admin
+# from django.urls import include, path
+
+# urlpatterns = [
+#     path("supersecret/", admin.site.urls),
+#     path("api/v1/auth/", include("djoser.urls")),
+#     path("api/v1/auth/", include("djoser.urls.jwt")),
+#     path("api/v1/profile/", include("apps.profiles.urls")),
+#     path("api/v1/properties/", include("apps.properties.urls")),
+#     path("api/v1/ratings/", include("apps.ratings.urls")),
+#     path("api/v1/enquiries/", include("apps.enquiries.urls")),
+# ]
+
+
+admin.site.site_header = "Microfinex Admin"
+admin.site.site_title = "Microfinex Admin Portal"
+admin.site.index_title = "Welcome to the Microfinex Portal"
